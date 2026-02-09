b0138ceea73957b65d272e1ece5339fb4701ab7b
Wood465 <1jan.lesjak@gmail.com>
2026-02-09 21:41:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿(() => {
+  "use strict";
+
+  const form = document.getElementById("reservationForm");
+  if (!form) return;
+
+  form.addEventListener("submit", (event) => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      event.preventDefault();
+      alert("Hvala! Vaša rezervacija je bila poslana.");
+      form.reset();
+    }
+
+    form.classList.add("was-validated");
+  });
+})();
